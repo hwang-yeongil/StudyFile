042dc4c357832d40ac395b3456af79fa7f8ed2bc
hwang-yeongil <71959055+hwang-yeongil@users.noreply.github.com>
2022-04-04 18:01:26 +0900

    정석1 498p

---
+++ src/DayB1_0404/WrapperEx2.java
@@ -0,0 +1,38 @@
+package DayB1_0404;
+
+public class WrapperEx2 {
+	public static void main(String[] args) {
+		int i = new Integer("100").intValue();
+		int i2= Integer.parseInt("100");
+		Integer i3= Integer.valueOf("100");
+		
+		int i4 = Integer.parseInt("100",2);
+		int i5 = Integer.parseInt("100",8);
+		int i6 = Integer.parseInt("100",16);
+		int i7 = Integer.parseInt("FF",16);
+//		int i8 = Integer.parseInt("FF");  NumberFormatException
+		
+		int i9 = Integer.valueOf("100",2);
+		int i10 = Integer.valueOf("100",8);
+		int i11 = Integer.valueOf("100",16);
+		int i12 = Integer.valueOf("FF",16);
+//		int i13 = Integer.valueOf("FF");  NumberFormatException
+		
+		System.out.println(i);
+		System.out.println(i2);
+		System.out.println(i3);
+		System.out.println(i4);
+		System.out.println(i5);
+		System.out.println(i6);
+		System.out.println(i7);
+
+		System.out.println(i9);
+		System.out.println(i10);
+		System.out.println(i11);
+		System.out.println(i12);
+		
+		
+		
+		
+	}
+}
